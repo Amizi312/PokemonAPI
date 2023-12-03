6cffdc95265017c2ca402d9e06bc3e928ffe468a
Amichai <aamizii@gmail.com>
2023-12-03 18:37:07 +0200

    1st task

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
 async function bringThemAll(){
-    const pokeList = await fetch ("https://pokeapi.co/api/v2/pokemon")
-    console.log(pokeList)
-    
+    const allPokeList = await fetch ("https://pokeapi.co/api/v2/pokemon?offset=0&limit=50")
+    const pokeList = await allPokeList.json()
+    displayPokes(pokeList)
+}
+
+function displayPokes(pokemons) {
+    const pokeList = document.getElementById("pokeList")
+    let html = "<ul>"
+    for(const pokemon of pokemons.results) {
+        const li = `
+        <li> 
+            ${pokemon.name}
+        </li>
+        `
+        html += li
+    }
+    html += "</ul>"
+    pokeList.innerHTML = html
 }
